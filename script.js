5e66e8e77eafd765d779fc60bf5feaa8b616ea10
DIVYANSHU KUMAR MAURYA <155415613+Dvyanshu1050@users.noreply.github.com>
2026-01-30 00:00:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+const getSumBtn = document.createElement("button");
+getSumBtn.innerText = "Get Total Price";
+document.body.appendChild(getSumBtn);
+
 const getSum = () => {
   const prices = document.querySelectorAll(".price");
   let total = 0;
 
-  prices.forEach((price) => {
-    total += Number(price.textContent);
+  prices.forEach((p) => {
+    total += parseInt(p.innerText);
   });
 
   const table = document.querySelector("table");
 
-
+  // prevent duplicate row
   if (document.getElementById("total-row")) return;
 
-  const totalRow = document.createElement("tr");
-  totalRow.id = "total-row";
+  const tr = document.createElement("tr");
+  tr.id = "total-row";
 
-  const totalCell = document.createElement("td");
-  totalCell.colSpan = 2;
-  totalCell.textContent = "Total Price = " + total;
+  const td = document.createElement("td");
+  td.colSpan = 2;
+  td.innerText = "Total Price = " + total;
 
-  totalRow.appendChild(totalCell);
-  table.appendChild(totalRow);
+  tr.appendChild(td);
+  table.appendChild(tr);
 };
+
+getSumBtn.addEventListener("click", getSum);
